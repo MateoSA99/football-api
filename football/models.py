021b8027346a272e74ea2ffc5f7c1c3b2043bb48
mateosotoa <mateo.soto@symplifica.com>
2023-07-08 16:06:20 -0500

    models and migrations

--- football/models.py
+++ football/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=20)
+    stadium = models.CharField(max_length=50)
+    titles = models.IntegerField(default=0)
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    country = models.CharField(max_length=20)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    
