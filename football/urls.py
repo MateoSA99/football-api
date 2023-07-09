db5deb008147165a12265607d8ff6005f20fbe87
mateosotoa <mateo.soto@symplifica.com>
2023-07-08 20:57:33 -0500

    view teams per country

--- football/urls.py
+++ football/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from football.views import TeamViewSet, PlayerViewSet
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from football.views import TeamViewSet, PlayerViewSet, TeamsPerCountryViewSet
 
 router = DefaultRouter()
 router.register(r'teams',TeamViewSet, basename='teams')
 router.register(r'players',PlayerViewSet, basename='players')
 
 
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('teams/country/<str:country>', TeamsPerCountryViewSet.as_view({'get':'list'})),
+    path('', include(router.urls)),
+    
 ]
 
--- football/views.py
+++ football/views.py
@@ -9,6 +9,14 @@ class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+class TeamsPerCountryViewSet(viewsets.ModelViewSet):
+    serializer_class = TeamSerializer
+
+    def get_queryset(self):
+        country = self.kwargs['country']
+        queryset = Team.objects.filter(country=country)
+        return queryset
+
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
